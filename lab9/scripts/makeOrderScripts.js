dfdec638c1b7e6f4b76b81a2a1e0fdf3f7391160
Alexeyts0Y <tsoyalexey331@gmail.com>
2024-12-08 23:23:42 +0300

    added 9th lab and fixed 8th

---
+++ lab9/scripts/makeOrderScripts.js
@@ -0,0 +1,393 @@
+"use strict";
+
+import { getDishById, createOrder } from "../scripts/dishesApi.js";
+
+const modal = document.getElementById("modal");
+
+let soupChoice;
+let mainChoice;
+let drkChoice;
+let sldChoice;
+let dsrtChoice;
+
+const dishCards = document.getElementById("dish-block");
+
+const resetButton = document.getElementById("reset");
+let dishes = [];
+
+function toNumber(stringNumber) {
+    let number = Number(stringNumber);
+    if (!number) {
+        number = 0;
+    }
+    return number;
+};
+
+function chooseDish(dish, mode) {
+    const dishName = dish.querySelector(".dish_name").textContent;
+    const dishPrice = dish.querySelector(".price").textContent;
+    
+    const value = dishName + ` ${dishPrice}`;
+    const keyword = `${dish.dataset.dish}`;
+    if (mode) {
+        switch (dish.dataset.category) {
+        case "soup":
+            document.getElementById("soupPlaceholder")
+                .setAttribute("value", value);
+            document.getElementById("soup_id")
+                .setAttribute("value", window.localStorage.getItem("soupId"));
+            break;
+        case "main-course":
+            document.getElementById("mainPlaceholder")
+                .setAttribute("value", value);
+            document.getElementById("main_course_id")
+                .setAttribute("value", window.localStorage.getItem("mainId"));
+            break;
+        case "drink":
+            document.getElementById("drkPlaceholder")
+                .setAttribute("value", value);
+            document.getElementById("drink_id")
+                .setAttribute("value", window.localStorage.getItem("drinkId"));
+            break;
+        case "salad":
+            document.getElementById("sldPlaceholder")
+                .setAttribute("value", value);
+            document.getElementById("salad_id")
+                .setAttribute("value", window.localStorage.getItem("saladId"));
+            break;
+        case "dessert":
+            document.getElementById("dsrtPlaceholder")
+                .setAttribute("value", value);
+            document.getElementById("dessert_id")
+                .setAttribute("value", window.localStorage
+                    .getItem("dessertId"));
+            break;
+        }
+        return;
+    }
+    switch (dish.dataset.category) {
+    case "soup":
+        document.getElementById("soupPlaceholder")
+            .setAttribute("value", "");
+        document.getElementById("soupChoice")
+            .setAttribute("value", "");
+        break;
+    case "main-course":
+        document.getElementById("mainPlaceholder")
+            .setAttribute("value", "");
+        document.getElementById("mainChoice")
+            .setAttribute("value", "");
+        break;
+    case "drink":
+        document.getElementById("drkPlaceholder")
+            .setAttribute("value", "");
+        document.getElementById("drinkChoice")
+            .setAttribute("value", "");
+        break;
+    case "salad":
+        document.getElementById("sldPlaceholder")
+            .setAttribute("value", "");
+        document.getElementById("saladChoice")
+            .setAttribute("value", "");
+        break;
+    case "dessert":
+        document.getElementById("dsrtPlaceholder")
+            .setAttribute("value", "");
+        document.getElementById("dessertChoice")
+            .setAttribute("value", "");
+        break;
+    }
+};
+
+function countPrice() {
+    let chosenSoupPrice;
+    let chosenDrinkPrice;
+    let chosenMainPrice;
+    let chosenSaladPrice;
+    let chosenDessertPrice;
+
+    let soupPrice, mainPrice, drkPrice, dsrtPrice, sldPrice;
+
+    let price1stPart, price2ndPart, price;
+
+    let soupPriceContent;
+    let mainPriceContent;
+    let drkPriceContent;
+    let sldPriceContent;
+    let dsrtPriceContent;
+    
+    if (soupChoice) {
+        chosenSoupPrice = Number(soupPrice);
+        soupPriceContent = (soupChoice.querySelector(".price").textContent);
+        soupPrice = soupPriceContent.slice(0, -1);
+    }
+    
+    if (mainChoice) {
+        chosenMainPrice = Number(mainPrice);
+        mainPriceContent = (mainChoice.querySelector(".price").textContent);
+        mainPrice = mainPriceContent.slice(0, -1);
+    }
+    
+    if (drkChoice) {
+        chosenDrinkPrice = Number(drkPrice);
+        drkPriceContent = (drkChoice.querySelector(".price").textContent);
+        drkPrice = drkPriceContent.slice(0, -1);
+    }
+    
+    if (dsrtChoice) {
+        chosenDessertPrice = Number(dsrtPrice);
+        dsrtPriceContent = (dsrtChoice.querySelector(".price").textContent);
+        dsrtPrice = dsrtPriceContent.slice(0, -1);
+    }
+    
+    if (sldChoice) {
+        chosenSaladPrice = Number(sldPrice);
+        sldPriceContent = (sldChoice.querySelector(".price").textContent);
+        sldPrice = sldPriceContent.slice(0, -1);
+    }
+    
+    chosenSoupPrice = toNumber(soupPrice);
+    chosenMainPrice = toNumber(mainPrice);
+    chosenDrinkPrice = toNumber(drkPrice);
+    chosenSaladPrice = toNumber(sldPrice);
+    chosenDessertPrice = toNumber(dsrtPrice);
+
+    price1stPart = chosenDrinkPrice + chosenSoupPrice + chosenMainPrice;
+    price2ndPart = chosenSaladPrice + chosenDessertPrice;
+
+    price = String(price1stPart + price2ndPart) + "₽";
+
+    document.querySelector(".fullPrice").textContent = price;
+}
+
+function fillDishCard(dishCard, dishData) {
+    dishCard.querySelector(".dish_img img").src = dishData.image;
+    dishCard.querySelector(".dish_img img").alt = dishData.name;
+    dishCard.querySelector(".price").textContent = `${dishData.price}₽`;
+    dishCard.querySelector(".dish_name").textContent = dishData.name;
+    dishCard.querySelector(".weight").textContent = dishData.count;
+}
+
+function createDishCard() {
+    let dishCard = document.createElement("div");
+    let figure = document.createElement("figure");
+    let img = document.createElement("img");
+    let weigth = document.createElement("p");
+    let price = document.createElement("p");
+    let dishName = document.createElement("p");
+    let div = document.createElement("div");
+    let button = document.createElement("button");
+    
+    dishCard.className = "dish-card";
+    figure.className = "dish_img";
+    weigth.className = "weight";
+    price.className = "price";
+    dishName.className = "dish_name";
+    
+    button.textContent = "Удалить";
+
+    figure.append(img);
+    div.append(weigth);
+    div.append(button);
+
+    dishCard.append(figure);
+    dishCard.append(price);
+    dishCard.append(dishName);
+    dishCard.append(div);
+
+    return dishCard;
+}
+
+const localStorageSize = window.localStorage.length;
+
+function displayNothingChosen() {
+    if (dishes.length) {
+        document.querySelector(".nothingChosenLabel1")
+            .style.display = "none";
+        document.querySelector(".nothingChosenLabel2")
+            .style.display = "none";
+        return;
+    }
+    document.querySelector(".nothingChosenLabel1").style.display = "inline";
+    document.querySelector(".nothingChosenLabel2").style.display = "inline";
+}
+
+function renderDishes(arr) {
+    displayNothingChosen();
+    dishCards.innerHTML = "";
+    dishes.forEach((dish) => {
+        let dishCard = createDishCard();
+        dishCard.setAttribute("data-dish", `${dish.keyword}`);
+        dishCard.setAttribute("data-id", `${dish.id}`);
+        dishCard.setAttribute("data-category", `${dish.category}`);
+        fillDishCard(dishCard, dish);
+        dishCards.append(dishCard);
+        if (dish.category == "soup") {
+            soupChoice = dishCard;
+        } else if (dish.category == "main-course") {
+            mainChoice = dishCard;
+        } else if (dish.category == "drink") {
+            drkChoice = dishCard;
+        } else if (dish.category == "salad") {
+            sldChoice = dishCard;
+        } else if (dish.category == "dessert") {
+            dsrtChoice = dishCard;
+        }
+        chooseDish(dishCard, true);
+    });
+    countPrice();
+}
+
+function removeDishData(category) {
+    for (let i = 0; i < dishes.length; i++) {
+        if (dishes[i].category == category) {
+            dishes.splice(i, 1);
+        }
+    }
+}
+
+console.log(window.localStorage);
+
+const promises = [];
+
+for (let i = 0; i < localStorageSize; i++) {
+    let key = window.localStorage.key(i);
+    let id = window.localStorage.getItem(key);
+    if (id == "") continue;    
+    promises.push(getDishById(id).then(data => {
+        dishes.push(data);
+    }));
+}
+
+Promise.all(promises).then(() => {
+    renderDishes();
+});
+
+function removeDishCard(dishCard) {
+    switch (dishCard.dataset.category) {
+    
+    case "soup":
+        window.localStorage.removeItem("soupId");
+        removeDishData("soup");
+        soupChoice = undefined;
+        break;
+
+    case "main-course":
+        window.localStorage.removeItem("mainId");
+        removeDishData("main-course");
+        mainChoice = undefined;
+        break;
+
+    case "salad":
+        window.localStorage.removeItem("saladId");
+        removeDishData("salad");
+        sldChoice = undefined;
+        break;
+
+    case "drink":
+        window.localStorage.removeItem("drinkId");
+        removeDishData("drink");
+        drkChoice = undefined;
+        break;
+
+    case "dessert":
+        window.localStorage.removeItem("dessertId");
+        removeDishData("dessert");
+        dsrtChoice = undefined;
+        break;
+    }
+}
+
+dishCards.onclick = function(event) {
+    let target = event.target;
+  
+    if (target.tagName != "BUTTON") return;
+  
+    const dishCard = target.parentElement.parentElement;
+
+    removeDishCard(dishCard);
+    renderDishes();
+    chooseDish(dishCard, false);
+    countPrice();
+};
+
+resetButton.onclick = function() {
+    soupChoice = undefined;
+    mainChoice = undefined;
+    drkChoice = undefined;
+    sldChoice = undefined;
+    dsrtChoice = undefined;
+
+    countPrice();
+};
+
+function resetAll() {
+    soupChoice = undefined;
+    mainChoice = undefined;
+    drkChoice = undefined;
+    sldChoice = undefined;
+    dsrtChoice = undefined;
+    window.localStorage.clear();
+    dishes = [];
+}
+
+function displayModal(text) {
+    const window = document.getElementById("window");
+    const p = window.querySelector(".text");
+    
+    p.textContent = text;
+    modal.className = "modal active";
+}
+
+document.querySelector(".orderForm")
+    .addEventListener("submit", async function(e) {
+        e.preventDefault();
+
+        const soupChoice = window.localStorage.getItem("soupId");
+        const mainChoice = window.localStorage.getItem("mainId");
+        const drkChoice = window.localStorage.getItem("drinkId");
+        const sldChoice = window.localStorage.getItem("saladId");
+        const dsrtChoice = window.localStorage.getItem("dessertId");
+
+        if (!soupChoice && !mainChoice && !sldChoice && 
+            !drkChoice && !dsrtChoice) {
+            displayModal("Ничего не выбрано. Выберите блюда для заказа");
+            return false;
+        }
+
+        if ((soupChoice || mainChoice || sldChoice) && !drkChoice) {
+            displayModal("Выберите напиток");
+            return false;
+        }
+
+        if (soupChoice && !mainChoice) {
+            displayModal("Выберите главное блюдо/салат/стартер");
+            return false;
+        }
+
+        if (sldChoice && (!mainChoice && !soupChoice)) {
+            displayModal("Выберите суп или главное блюдо");
+            return false;
+        }
+
+        if (!mainChoice && (drkChoice || dsrtChoice)) {
+            displayModal("Выберите главное блюдо");
+            return false;
+        }
+
+        const formData = new FormData(this);
+        const response = await createOrder({
+            body: formData
+        });
+        if (!response) {
+            throw new Error("Ошибка создания заказа");
+        }
+        resetAll();
+        renderDishes();
+    });
+
+const okayBtn = document.querySelector(".okayBtn");
+
+okayBtn.onclick = function() {
+    modal.className = "modal";
+};
---
+++ lab9/scripts/scripts.js
@@ -0,0 +1,457 @@
+"use strict";
+
+import { getAllDishes } from "./dishesApi.js";
+
+window.localStorage.setItem("soupId", "");
+window.localStorage.setItem("mainId", "");
+window.localStorage.setItem("drinkId", "");
+window.localStorage.setItem("saladId", "");
+window.localStorage.setItem("dessertId", "");
+
+let soupChoiceFilter;
+let mainChoiceFilter;
+let drkChoiceFilter;
+let sldChoiceFilter;
+let dsrtChoiceFilter;
+
+let isActive1 = false;
+let isActive2 = false;
+let isActive3 = false;
+let isActive4 = false;
+let isActive5 = false;
+
+let soupChoice;
+let mainChoice;
+let drkChoice;
+let sldChoice;
+let dsrtChoice;
+
+const soupFilters = document.getElementById("soupFilters");
+const mainFilters = document.getElementById("mainFilters");
+const drinkFilters = document.getElementById("drinkFilters");
+const saladFilters = document.getElementById("saladFilters");
+const dessertFilters = document.getElementById("dessertFilters");
+
+const dishSoupsCards = document.getElementById("soupes");
+const dishMainCards = document.getElementById("main");
+const dishDrinksCards = document.getElementById("drinks");
+const dishSaladsCards = document.getElementById("salads");
+const dishDessertsCards = document.getElementById("desserts");
+
+let dishes;
+
+function fillDishCard(dishCard, dishData) {
+    dishCard.querySelector(".dish_img img").src = dishData.image;
+    dishCard.querySelector(".dish_img img").alt = dishData.name;
+    dishCard.querySelector(".price").textContent = `${dishData.price}₽`;
+    dishCard.querySelector(".dish_name").textContent = dishData.name;
+    dishCard.querySelector(".weight").textContent = dishData.count;
+}
+
+function createDishCard() {
+    let dishCard = document.createElement("div");
+    let figure = document.createElement("figure");
+    let img = document.createElement("img");
+    let weigth = document.createElement("p");
+    let price = document.createElement("p");
+    let dishName = document.createElement("p");
+    let div = document.createElement("div");
+    let button = document.createElement("button");
+    
+    dishCard.className = "dish-card";
+    figure.className = "dish_img";
+    weigth.className = "weight";
+    price.className = "price";
+    dishName.className = "dish_name";
+    
+    button.textContent = "Добавить";
+
+    figure.append(img);
+    div.append(weigth);
+    div.append(button);
+
+    dishCard.append(figure);
+    dishCard.append(price);
+    dishCard.append(dishName);
+    dishCard.append(div);
+
+    return dishCard;
+}
+
+getAllDishes()
+    .then(responseDishes => {
+        dishes = responseDishes;
+        responseDishes.forEach((dish) => {
+            let dishCard = createDishCard();
+        
+            switch (dish.category) {
+            case "soup":
+                dishCard.setAttribute("data-dish", `${dish.keyword}`);
+                dishCard.setAttribute("data-id", `${dish.id}`);
+                fillDishCard(dishCard, dish);
+                dishSoupsCards.append(dishCard);
+                break;
+        
+            case "main-course":
+                dishCard.setAttribute("data-dish", `${dish.keyword}`);
+                dishCard.setAttribute("data-id", `${dish.id}`);
+                fillDishCard(dishCard, dish);
+                dishMainCards.append(dishCard);
+                break;
+        
+            case "salad":
+                dishCard.setAttribute("data-dish", `${dish.keyword}`);
+                dishCard.setAttribute("data-id", `${dish.id}`);
+                fillDishCard(dishCard, dish);
+                dishSaladsCards.append(dishCard);
+                break;
+            
+            case "drink":
+                dishCard.setAttribute("data-dish", `${dish.keyword}`);
+                dishCard.setAttribute("data-id", `${dish.id}`);
+                fillDishCard(dishCard, dish);
+                dishDrinksCards.append(dishCard);
+                break;
+        
+            case "dessert":
+                dishCard.setAttribute("data-dish", `${dish.keyword}`);
+                dishCard.setAttribute("data-id", `${dish.id}`);
+                fillDishCard(dishCard, dish);
+                dishDessertsCards.append(dishCard);
+                break;
+            }
+        });
+    });
+
+function toNumber(stringNumber) {
+    let number = Number(stringNumber);
+    if (!number) {
+        number = 0;
+    }
+    return number;
+};
+
+function countPrice() {
+    let chosenSoupPrice;
+    let chosenDrinkPrice;
+    let chosenMainPrice;
+    let chosenSaladPrice;
+    let chosenDessertPrice;
+
+    let soupPrice, mainPrice, drkPrice, dsrtPrice, sldPrice;
+
+    let price1stPart, price2ndPart, price;
+
+    let soupPriceContent;
+    let mainPriceContent;
+    let drkPriceContent;
+    let sldPriceContent;
+    let dsrtPriceContent;
+    
+    if (soupChoice) {
+        chosenSoupPrice = Number(soupPrice);
+        soupPriceContent = (soupChoice.querySelector(".price").textContent);
+        soupPrice = soupPriceContent.slice(0, -1);
+    }
+    
+    if (mainChoice) {
+        chosenMainPrice = Number(mainPrice);
+        mainPriceContent = (mainChoice.querySelector(".price").textContent);
+        mainPrice = mainPriceContent.slice(0, -1);
+    }
+    
+    if (drkChoice) {
+        chosenDrinkPrice = Number(drkPrice);
+        drkPriceContent = (drkChoice.querySelector(".price").textContent);
+        drkPrice = drkPriceContent.slice(0, -1);
+    }
+    
+    if (dsrtChoice) {
+        chosenDessertPrice = Number(dsrtPrice);
+        dsrtPriceContent = (dsrtChoice.querySelector(".price").textContent);
+        dsrtPrice = dsrtPriceContent.slice(0, -1);
+    }
+    
+    if (sldChoice) {
+        chosenSaladPrice = Number(sldPrice);
+        sldPriceContent = (sldChoice.querySelector(".price").textContent);
+        sldPrice = sldPriceContent.slice(0, -1);
+    }
+    
+    chosenSoupPrice = toNumber(soupPrice);
+    chosenMainPrice = toNumber(mainPrice);
+    chosenDrinkPrice = toNumber(drkPrice);
+    chosenSaladPrice = toNumber(sldPrice);
+    chosenDessertPrice = toNumber(dsrtPrice);
+
+    price1stPart = chosenDrinkPrice + chosenSoupPrice + chosenMainPrice;
+    price2ndPart = chosenSaladPrice + chosenDessertPrice;
+
+    price = String(price1stPart + price2ndPart) + "₽";
+
+    document.querySelector(".fullPrice").textContent = price;
+}
+
+function showPanel() {
+    const window = document.getElementById("goToOrder");
+    window.className = "goToOrder active";
+}
+
+function displayModal(text) {
+    const window = document.getElementById("window");
+    const p = window.querySelector(".text");
+    
+    p.textContent = text;
+    modal.className = "modal active";
+}
+
+function chooseDish(dish) {
+    const dishName = dish.querySelector(".dish_name").textContent;
+    const dishPrice = dish.querySelector(".price").textContent;
+    
+    const value = dishName + ` ${dishPrice}`;
+    const keyword = `${dish.dataset.dish}`;
+    
+    if (dish.parentElement.getAttribute("id") == "soupes") {
+        document.getElementById("soupPlaceholder").setAttribute("value", value);
+    } else if (dish.parentElement.getAttribute("id") == "main") {
+        document.getElementById("mainPlaceholder").setAttribute("value", value);
+    } else if (dish.parentElement.getAttribute("id") == "drinks") {
+        document.getElementById("drkPlaceholder").setAttribute("value", value);
+    } else if (dish.parentElement.getAttribute("id") == "salads") {
+        document.getElementById("sldPlaceholder").setAttribute("value", value);
+    } else if (dish.parentElement.getAttribute("id") == "desserts") {
+        document.getElementById("dsrtPlaceholder").setAttribute("value", value);
+    }
+};
+
+function highlightCard(target, selected) {
+    if (selected) {
+        selected.style.border = "2px solid white";
+    }
+    target.style.border = "2px solid tomato";
+};
+
+function highlightFilter(target, selected) {
+    if (selected) {
+        selected.style.backgroundColor = "white";
+    }
+    target.style.backgroundColor = "lightblue";
+}
+
+document.getElementById("linkToOrder").onclick = function checkConditions(e) {
+    if (!soupChoice && !mainChoice && !sldChoice && !drkChoice && !dsrtChoice) {
+        e.preventDefault();
+        displayModal("Ничего не выбрано. Выберите блюда для заказа");
+        return false;
+    } else if ((soupChoice || mainChoice || sldChoice) && !drkChoice) {
+        e.preventDefault();
+        displayModal("Выберите напиток");
+        return false;
+    } else if (soupChoice && !mainChoice) {
+        e.preventDefault();
+        displayModal("Выберите главное блюдо/салат/стартер");
+        return false;
+    } else if (sldChoice && (!mainChoice && !soupChoice)) {
+        e.preventDefault();
+        displayModal("Выберите суп или главное блюдо");
+        return false;
+    } else if (!mainChoice && (drkChoice || dsrtChoice)) {
+        e.preventDefault();
+        displayModal("Выберите главное блюдо");
+        return false;
+    }
+};
+
+dishSoupsCards.onclick = function(event) {
+    let target = event.target;
+  
+    if (target.tagName != "BUTTON") return;
+  
+    const dishCard = target.parentElement.parentElement;
+    highlightCard(dishCard, soupChoice);
+    soupChoice = dishCard;
+    window.localStorage.setItem("soupId", `${dishCard.dataset.id}`);
+
+    showPanel();
+    chooseDish(dishCard);
+    countPrice();
+};
+
+dishMainCards.onclick = function(event) {
+    let target = event.target;
+    
+    if (target.tagName != "BUTTON") return;
+    
+    const dishCard = target.parentElement.parentElement;
+    highlightCard(dishCard, mainChoice);
+    mainChoice = dishCard;
+    window.localStorage.setItem("mainId", `${dishCard.dataset.id}`);
+    
+    showPanel();
+    chooseDish(dishCard);
+    countPrice();
+};
+
+dishDrinksCards.onclick = function(event) {
+    let target = event.target;
+    
+    if (target.tagName != "BUTTON") return;
+    
+    const dishCard = target.parentElement.parentElement;
+    highlightCard(dishCard, drkChoice);
+    drkChoice = dishCard;
+    window.localStorage.setItem("drinkId", `${dishCard.dataset.id}`);
+    
+    showPanel();
+    chooseDish(dishCard);
+    countPrice();
+};
+
+dishSaladsCards.onclick = function(event) {
+    let target = event.target;
+    
+    if (target.tagName != "BUTTON") return;
+    
+    const dishCard = target.parentElement.parentElement;
+    highlightCard(dishCard, sldChoice);
+    sldChoice = dishCard;
+    window.localStorage.setItem("saladId", `${dishCard.dataset.id}`);
+    
+    showPanel();
+    chooseDish(dishCard);
+    countPrice();
+};
+
+dishDessertsCards.onclick = function(event) {
+    let target = event.target;
+    
+    if (target.tagName != "BUTTON") return;
+    
+    const dishCard = target.parentElement.parentElement;
+    highlightCard(dishCard, dsrtChoice);
+    dsrtChoice = dishCard;
+    window.localStorage.setItem("dessertId", `${dishCard.dataset.id}`);
+
+    showPanel();
+    chooseDish(dishCard);
+    countPrice();
+};
+
+function resetFilter(selectedFilter, cards, target) {
+    selectedFilter = undefined;
+    cards.forEach(dishCard => {
+        dishCard.style.display = "flex";
+    });
+    target.style.backgroundColor = "white";
+}
+
+//Фильтрация блюд
+function filterDishes(cards, selectedFilter) {
+    cards.forEach(dishCard => {
+        const keyword = dishCard.dataset.dish;
+        const dishData = dishes.find(dish => dish.keyword == keyword);
+
+        if (dishData.kind == selectedFilter.dataset.kind) {
+            dishCard.style.display = "flex";
+        } else {
+            dishCard.style.display = "none";
+        }
+    });
+}
+
+soupFilters.onclick = function(event) {
+    let target = event.target;
+    let cards = dishSoupsCards.querySelectorAll(".dish-card");
+    
+    if (target.tagName != "BUTTON") return;
+    
+    if (isActive1 && (target === soupChoiceFilter)) {
+        isActive1 = false;
+        resetFilter(soupChoiceFilter, cards, target);
+        return;
+    }
+
+    isActive1 = true;
+    highlightFilter(target, soupChoiceFilter);
+    soupChoiceFilter = target;
+    filterDishes(cards, soupChoiceFilter);
+};
+
+mainFilters.onclick = function(event) {
+    let target = event.target;
+    let cards = dishMainCards.querySelectorAll(".dish-card");
+    
+    if (target.tagName != "BUTTON") return;
+    
+    if (isActive2 && (target === mainChoiceFilter)) {
+        isActive2 = false;
+        resetFilter(mainChoiceFilter, cards, target);
+        return;
+    }
+
+    isActive2 = true;
+    highlightFilter(target, mainChoiceFilter);
+    mainChoiceFilter = target;
+    filterDishes(cards, mainChoiceFilter);
+};
+
+drinkFilters.onclick = function(event) {
+    let target = event.target;
+    let cards = dishDrinksCards.querySelectorAll(".dish-card");
+    
+    if (target.tagName != "BUTTON") return;
+    
+    if (isActive3 && (target === drkChoiceFilter)) {
+        isActive3 = false;
+        resetFilter(drkChoiceFilter, cards, target);
+        return;
+    }
+
+    isActive3 = true;
+    highlightFilter(target, drkChoiceFilter);
+    drkChoiceFilter = target;
+    filterDishes(cards, drkChoiceFilter);
+};
+
+saladFilters.onclick = function(event) {
+    let target = event.target;
+    let cards = dishSaladsCards.querySelectorAll(".dish-card");
+    
+    if (target.tagName != "BUTTON") return;
+    
+    if (isActive4 && (target === sldChoiceFilter)) {
+        isActive4 = false;
+        resetFilter(sldChoiceFilter, cards, target);
+        return;
+    }
+
+    isActive4 = true;
+    highlightFilter(target, sldChoiceFilter);
+    sldChoiceFilter = target;
+    filterDishes(cards, sldChoiceFilter);
+};
+
+dessertFilters.onclick = function(event) {
+    let target = event.target;
+    let cards = dishDessertsCards.querySelectorAll(".dish-card");
+    
+    if (target.tagName != "BUTTON") return;
+    
+    if (isActive5 && (target === dsrtChoiceFilter)) {
+        isActive5 = false;
+        resetFilter(dsrtChoiceFilter, cards, target);
+        return;
+    }
+
+    isActive5 = true;
+    highlightFilter(target, dsrtChoiceFilter);
+    dsrtChoiceFilter = target;
+    filterDishes(cards, dsrtChoiceFilter);
+};
+
+const okayBtn = document.querySelector(".okayBtn");
+
+okayBtn.onclick = function() {
+    modal.className = "modal";
+};
